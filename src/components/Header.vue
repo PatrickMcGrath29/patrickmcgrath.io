acf1a5c207f4f274157804d85a06522468d529cd
Patrick McGrath <mcgrath.p@husky.neu.edu>
2018-02-03 17:15:40 -0500

    Funky new navbar icon

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,12 @@
 <template>
   <header role="banner">
     <router-link v-bind:to="{ name: 'Home' }" class="header-icon">Patrick R. McGrath</router-link>
-    <div class="navbar-collapse-toggle hamburger" v-on:click="toggleMobileNav">
+    <div class="navbar-collapse-toggle hamburger" :class="{'navbar-active': !mobileHidden}" v-on:click="toggleMobileNav">
       <div class="hamburger-line"></div>
       <div class="hamburger-line"></div>
       <div class="hamburger-line"></div>
     </div>
-    <nav class="navbar navbar-collapse" :class="{'mobile-hidden': mobileHidden}" ref="navbar">
+    <nav class="navbar" :class="{'mobile-hidden': mobileHidden}">
       <router-link v-bind:to="{ name: 'Home' }" active-class="active" class="navbar-link">Home</router-link>
       <a href="#" class="navbar-link">Projects</a>
       <a href="/static/resume.pdf" class="navbar-link">Resume</a>
