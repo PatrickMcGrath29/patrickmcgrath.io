87e456ded63aa367e6195ad691643a8ceccc2228
Patrick McGrath <mcgrath.p@husky.neu.edu>
2018-03-23 16:14:38 -0400

    Updating resume version

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -8,7 +8,7 @@
     </div>
     <nav class="navbar" :class="{'mobile-hidden': mobileHidden}">
       <router-link v-bind:to="{ name: 'Home' }" active-class="active" class="navbar-link">Home</router-link>
-      <a href="/static/resume.pdf" class="navbar-link">Resume</a>
+      <a href="/static/Resume_McGrath_Patrick.pdf" class="navbar-link">Resume</a>
     </nav>
   </header>
 </template>
