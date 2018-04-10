7b99440bc610be9ebd15b403ca996af516261bf3
Patrick McGrath <mcgrath.p@husky.neu.edu>
2018-04-10 18:24:41 -0400

    Content update

--- src/pages/home/Index.vue
+++ src/pages/home/Index.vue
@@ -4,7 +4,7 @@
       <div class="homepage-intro content-section">
         <h1><span class="heading-bracket left">[</span>About<span class="heading-bracket right">]</span></h1>
         <p>
-          I am a student at Northeastern University pursuing a <strong>Bachelor of Science in Computer Science</strong>. I'm an engineer and entrepreneur at heart, driven by a keen desire to change the world through innovative problem solving. I appreciate every opportunity to disrupt the status quo in favor of progress.
+          I am a student at Northeastern University pursuing a <strong>Bachelor of Science in Computer Science</strong>. I'm an engineer and entrepreneur at heart, driven by a desire to change the world through innovative problem solving. I appreciate every opportunity to disrupt the status quo in favor of progress.
         </p>
         <p>
           In my spare time you'll find me hanging out with friends, working on side projects, and exploring Boston's restaurant scene.
