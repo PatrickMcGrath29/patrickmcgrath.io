694741d1fee46ea20ec4da5941aa790ac826b930
Patrick Rory McGrath <hello@patrickmcgrath.io>
2018-06-11 17:36:05 -0400

    adding blog stuff

---
+++ src/cms.js
@@ -0,0 +1,2 @@
+import Butter from 'buttercms'
+export const butter = Butter('173a276711a4901772ad07a7e18bc5b023d2ee78')
---
+++ src/pages/blog/Index.vue
@@ -0,0 +1,38 @@
+<template>
+  <default-template>
+    <div class="container">
+      <div class="content-section">
+        <h1><span class="heading-bracket left">[</span>Blog<span class="heading-bracket right">]</span></h1>
+      </div>
+    </div>
+  </default-template>
+</template>
+
+<script>
+import DefaultTemplate from '@/templates/Default'
+import { butter } from '@/cms'
+
+export default {
+  name: 'home-index',
+  components: {
+    DefaultTemplate
+  },
+  methods: {
+    fetchPosts () {
+      butter.post.list({
+        page: 1,
+        page_size: 10
+      }).then((res) => {
+        console.log('Content from ButterCMS')
+        console.log(res)
+      })
+    }
+  },
+  created () {
+    this.fetchPosts()
+  }
+}
+</script>
+<style lang="sass">
+@import "../../assets/styles/pages/_home-index.scss";
+</style>
