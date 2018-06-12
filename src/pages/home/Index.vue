5fe84be7464035ba58d71fc6e689636b5efad099
Patrick Rory McGrath <hello@patrickmcgrath.io>
2018-06-12 10:42:36 -0400

    Editting email svg, updating hobby content on homepage

--- src/pages/home/Index.vue
+++ src/pages/home/Index.vue
@@ -6,36 +6,44 @@
         <div class="bio-and-image">
           <div class="bio">
             <p>
-              I am a student at Northeastern University pursuing a <strong>Bachelor of Science in Computer Science</strong>. I'm an engineer and entrepreneur at heart, driven by a desire to change the world through innovative problem solving. I appreciate every opportunity to disrupt the status quo in favor of progress.
+              I am a student at Northeastern University pursuing a <strong>Bachelor of Science in Computer Science</strong>, currently working as a UI engineer at <a href="https://placepass.com">PlacePass</a>. I'm an engineer and entrepreneur at heart, driven by a desire to change the world through innovative problem solving. I appreciate every opportunity to disrupt the status quo in favor of progress.
             </p>
             <p>
-              In my spare time you'll find me hanging out with friends, working on side projects, and exploring Boston's restaurant scene.
+              In my spare time you'll find me hanging out with friends, hiking or skiing the northern east coast, working on side projects, and exploring Boston's restaurant scene.
             </p>
           </div>
           <img src="@/assets/images/patrickmcgrath.jpg" height="300px" width="auto" alt="Patrick McGrath">
         </div>
       </div>
       <div class="content-section">
-        <h1><span class="heading-bracket left">[</span>Current Work<span class="heading-bracket right">]</span></h1>
+        <h1><span class="heading-bracket left">[</span>My Hobbies<span class="heading-bracket right">]</span></h1>
         <div class="content-section-bordered">
           <h2> Victoria McGrath Foundation </h2>
           <p>
-            After my sister passed away in a car accident in March of 2016 my family and I decided to start a foundation in her memory. <a href="https://www.victoriamcgrath.org">The Victoria McGrath Foundation</a> serves to support ministries and foundations through financial giving, with a focus on special needs ministries and those that help refugees.
+            <a href="https://www.victoriamcgrath.org">The Victoria McGrath Foundation</a> foundation was created in 2018 to honor the memory of my sister, Victoria. Our objective is to serve others by creating pathways for hope and care, with a focus on supporting special needs ministries, displaced refugees, and veterans.
             <br><br>
             As both a board member and the lead developer I help guide the course of the foundation while building and maintaining its online assets.
           </p>
         </div>
         <div class="content-section-bordered">
+          <h2> South Africa 2018 </h2>
+          <p>
+            From mid-June to mid-July I will be heading on a mission trip to South Africa, where I will have the opportunity to teach foundational math, english, and computer science skills to local students. The trip, led by Cru, will bring us to Mamelodi South Africa where we will be partnering with the <a href="http://mamelodi.org/">Mamelodi Iniative</a>, a local Christian organization that aims to empower local students with the skills they need to inspire positive change.
+            <br><br>
+            If you'd like to get updates about the trip while I'm away shoot me an note at the email address below!
+          </p>
+        </div>
+        <div class="content-section-bordered">
           <h2> Agape Christian Fellowship </h2>
           <p>
-            I currently serve as an executive board member at <a href="https://www.facebook.com/groups/nuagape/">Agape Christian Fellowship</a>, a Christian organization at Northeastern University. Agape partners with Cru (an international Christian organization) to offer a student run environment where students can grow in community and understanding of Christ.
+            I currently serve as an executive board member at <a href="https://www.facebook.com/groups/nuagape/">Agape Christian Fellowship</a>, a Christian organization at Northeastern University. Agape partners with Cru (an international Christian organization) to offer a student run environment where students can grow in community and the understanding of Christ.
           </p>
         </div>
       </div>
       <div class="content-section">
         <h1><span class="heading-bracket left">[</span>Say Hello<span class="heading-bracket right">]</span></h1>
         <p>
-          I'm always interested in hearing about new projects or opportunities. I also enjoy casual conversations.
+          If you'd like to get in contact with me, drop me a line at the email address below.
         </p>
         <div class="social-icon__set">
           <a class="social-icon__icon green" href="mailto:hello@patrickmcgrath.io">
