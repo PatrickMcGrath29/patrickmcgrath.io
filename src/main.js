5ca6e0869e93ef376f31b8a4031998116f50d192
Patrick Rory McGrath <hello@patrickmcgrath.io>
2018-06-13 08:15:33 -0400

    Adding google tag manager basic implementation, upgrading yarn packages

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App'
+import VueGtm from 'vue-gtm'
 import router from './router'
 
 Vue.config.productionTip = false
@@ -9,4 +10,10 @@ new Vue({
   router,
   components: { App },
   template: '<App/>'
+})
+
+Vue.use(VueGtm, {
+  enabled: true,
+  debug: false,
+  vueRouter: router
 })
--- src/pages/home/Index.vue
+++ src/pages/home/Index.vue
@@ -73,7 +73,7 @@ export default {
     DefaultTemplate,
     EmailIcon,
     GithubIcon,
-    LinkedInIcon,
+    LinkedInIcon
   }
 }
 </script>
